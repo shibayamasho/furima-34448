9e6c660ef731f082bb772cb912f45fb07f88f6c0
shibayamasho <men.in.boss2@gmail.com>
2021-02-17 21:49:05 +0900

    activehashにバリデーションを追記

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,11 +13,15 @@ class Item < ApplicationRecord
     has_one_attached :image
     validates :item_name
     validates :item_text
-    validates :category_id
-    validates :condition_id
-    validates :delivery_fee_id
-    validates :shipment_source_id
-    validates :days_to_ship_id
+
+    with_options numericality: { other_than: 1 } do
+      validates :category_id
+      validates :condition_id
+      validates :delivery_fee_id
+      validates :shipment_source_id
+      validates :days_to_ship_id
+    end
+
     validates :price
   end
 end
