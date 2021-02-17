38b3395b79c10e117e537529ded2ea9d2fc1dddd
shibayamasho <men.in.boss2@gmail.com>
2021-02-17 18:10:41 +0900

    共通のバリデーションをwith_optionsでまとめた

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,22 +6,26 @@ class User < ApplicationRecord
   
   with_options presence: true do
 
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX # message: 'には英字と数字の両方を含めて設定してください'
+    PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+    validates_format_of :password, with: PASSWORD_REGEX # message: 'には英字と数字の両方を含めて設定してください'
 
-  validates :nickname
+    validates :nickname
 
-  NAME_REGEX = /\A[ぁ-んァ-ン一-龥々]+\z/
-  validates :last_name, format: { with: NAME_REGEX }
-  validates :first_name, format: { with: NAME_REGEX }
+    NAME_REGEX = /\A[ぁ-んァ-ン一-龥々]+\z/
+    with_options format: { with: NAME_REGEX } do
+      validates :last_name
+      validates :first_name
+    end
 
-  NAME_KATAKANA_REGEX = /\A[ァ-ヶー－]+\z/
-  validates :last_name_katakana, format: { with: NAME_KATAKANA_REGEX }
-  validates :first_name_katakana, format: { with: NAME_KATAKANA_REGEX }
+    NAME_KATAKANA_REGEX = /\A[ァ-ヶー－]+\z/
+    with_options format: { with: NAME_KATAKANA_REGEX } do
+      validates :last_name_katakana
+      validates :first_name_katakana
+    end
 
-  validates :birthday
+    validates :birthday
+  end
 
   # has_many :items
   # has_many :purchases
-  end
 end
