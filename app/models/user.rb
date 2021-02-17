1b270c761ecf565db1775c7862edcef90930ae2d
shibayamasho <men.in.boss2@gmail.com>
2021-02-17 17:21:17 +0900

    Rubocopを実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,19 +4,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX  # message: 'には英字と数字の両方を含めて設定してください' 
-  
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX # message: 'には英字と数字の両方を含めて設定してください'
+
   validates :nickname, presence: true
 
   NAME_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/
-  validates :last_name, presence: true, format: {with: NAME_REGEX}
-  validates :first_name, presence: true, format: {with: NAME_REGEX}
+  validates :last_name, presence: true, format: { with: NAME_REGEX }
+  validates :first_name, presence: true, format: { with: NAME_REGEX }
 
   NAME_KATAKANA_REGEX = /\A[ァ-ヶー－]+\z/
-  validates :last_name_katakana, presence: true, format: {with: NAME_KATAKANA_REGEX}
-  validates :first_name_katakana, presence: true, format: {with: NAME_KATAKANA_REGEX}
-  
+  validates :last_name_katakana, presence: true, format: { with: NAME_KATAKANA_REGEX }
+  validates :first_name_katakana, presence: true, format: { with: NAME_KATAKANA_REGEX }
+
   validates :birthday, presence: true
 
   # has_many :items
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {Faker::Name.initials(number: 2)}
-    email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6, mix_case: true)}
-    password_confirmation {password}
-    last_name             {"鈴木"}
-    first_name            {"一郎"}
-    last_name_katakana    {"スズキ"}
-    first_name_katakana   {"イチロー"}
-    birthday              {Faker::Date.between(from: '1930-01-01', to: '2016-12-31')}
+    nickname              { Faker::Name.initials(number: 2) }
+    email                 { Faker::Internet.free_email }
+    password              { Faker::Internet.password(min_length: 6, mix_case: true) }
+    password_confirmation { password }
+    last_name             { '鈴木' }
+    first_name            { '一郎' }
+    last_name_katakana    { 'スズキ' }
+    first_name_katakana   { 'イチロー' }
+    birthday              { Faker::Date.between(from: '1930-01-01', to: '2016-12-31') }
   end
 end
