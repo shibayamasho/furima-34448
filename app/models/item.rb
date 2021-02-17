17f617f8f711e8a6f1cf8ddd758d55acbc490ea5
shibayamasho <men.in.boss2@gmail.com>
2021-02-17 21:09:32 +0900

    itemsモデル バリエーションを記述① presence trueのみ

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,16 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  # has_one :purchase
+
+  with_options presence: true do
+    has_one_attached :image
+    validates :item_name
+    validates :item_text
+    validates :category_id
+    validates :condition_id
+    validates :delivery_fee_id
+    validates :shipment_source_id
+    validates :days_to_ship_id
+    validates :price
+  end
 end
