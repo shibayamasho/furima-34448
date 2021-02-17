c0a3dcf27da506e84101f1874c9a15505821ab78
shibayamasho <men.in.boss2@gmail.com>
2021-02-17 12:39:14 +0900

    application_controllerの記述ミスを修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  def :configure_permitted_parameters
+  def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :last_name_katakana, :first_name_katakana, :birthday])
   end
 
