8569c2bb795bbd1c14899011b71d260bd3c343b8
shibayamasho <men.in.boss2@gmail.com>
2021-02-18 16:27:14 +0900

    authenticate_user!メソッドを使う記述に変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,11 +3,7 @@ class ItemsController < ApplicationController
   end
 
   def new
-    if user_signed_in?
-      @item = Item.new
-    else
-      redirect_to new_user_session_path
-    end
+    @item = Item.new
   end
 
   def create
