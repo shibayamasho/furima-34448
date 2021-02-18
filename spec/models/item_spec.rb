d83e7f2e9543ae73316f31475f482bedd0819c75
shibayamasho <men.in.boss2@gmail.com>
2021-02-18 16:33:48 +0900

    〇〇_idが１では登録できないテストコードを追記

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -61,6 +61,36 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include "Days to ship can't be blank"
       end
 
+      it 'カテゴリーの情報(category_id)が1では登録できないこと' do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Category must be other than 1"
+      end
+
+      it '商品の状態についての情報(condition_id)が1では登録できないこと' do
+        @item.condition_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Condition must be other than 1"
+      end
+
+      it '配送料の負担についての情報(delicery_fee_id)が1では登録できないこと' do
+        @item.delivery_fee_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Delivery fee must be other than 1"
+      end
+
+      it '発送元の地域についての情報(shipment_source_id)が1では登録できないこと' do
+        @item.shipment_source_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Shipment source must be other than 1"
+      end
+
+      it '発送までの日数についての情報(days_to_ship_id)が1では登録できないこと' do
+        @item.days_to_ship_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Days to ship must be other than 1"
+      end
+
       it '販売価格についての情報が必須であること' do
         @item.price = nil
         @item.valid?
