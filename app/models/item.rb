34295ecfcb53b38d3618c940c5141471051f7624
shibayamasho <men.in.boss2@gmail.com>
2021-02-18 11:29:35 +0900

    itemモデルのバリデーションの記述方法を修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,13 +6,14 @@ class Item < ApplicationRecord
   belongs_to :shipment_source
   belongs_to :days_to_ship
 
+  has_one_attached :image
   belongs_to :user
   # has_one :purchase
 
   with_options presence: true do
-    has_one_attached :image
     validates :item_name
     validates :item_text
+    validates :image
 
     with_options numericality: { other_than: 1 } do
       validates :category_id
@@ -22,6 +23,6 @@ class Item < ApplicationRecord
       validates :days_to_ship_id
     end
 
-    validates :price, numericality: true, only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
   end
 end
