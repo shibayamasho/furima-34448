37d1b1101ff4b315e9aaa8d86afa689b86e942de
shibayamasho <men.in.boss2@gmail.com>
2021-02-22 19:43:57 +0900

    コントローラーの記述を修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -52,7 +52,7 @@ class ItemsController < ApplicationController
     params.require(:item).permit(
       :item_name, :item_text,
       :category_id, :condition_id, :delivery_fee_id,
-      :shipment_source_id, :days_to_ship_id,
+      :prefecture_id, :days_to_ship_id,
       :price, :image
     ).merge(user_id: current_user.id)
   end
