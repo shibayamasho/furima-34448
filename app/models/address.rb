4e726e87867edbc37b5fe179139480ea71cb48da
shibayamasho <men.in.boss2@gmail.com>
2021-02-22 19:23:08 +0900

    addressモデルのバリデーションを記述途中

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,12 @@
 class Address < ApplicationRecord
+  with_options presence: true do
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :prefecture_id
+    validates :municipalities
+    validates :address
+    validates :phone_number, format: {with: /\A[0-9]{10,11}+\z/}
+    validates :order_id
+  end
+
+  belongs_to :order
 end
