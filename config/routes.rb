6fe679b5524ec76d6165414d2fa1fe50f74cfbee
shibayamasho <men.in.boss2@gmail.com>
2021-02-22 15:56:29 +0900

    アクション名をnew→indexに修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
   resources :items, only: [:new, :create, :show, :edit, :update, :destroy]
-  resources :orders, only: [:new, :create]
+  resources :orders, only: [:index, :create]
 end
