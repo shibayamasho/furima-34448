6571c1571b06436649719851252b6ea40099413d
shibayamasho <men.in.boss2@gmail.com>
2021-02-23 22:05:23 +0900

    order_addressのFactoryBotを記述

--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :order_address do
-    
+    token          { 'tok_0000000000000000000000000000' }
+    postal_code    { '000-0000' }
+    prefecture_id  { Faker::Number.between(from: 2, to: 48) }
+    municipalities { '市町村' }
+    address        { '番地' }
+    building_name  { '建物名' }
+    phone_number   { Faker::Number.between(from: 1000000000, to: 99999999999) }
+    association :user
+    association :item
   end
 end
