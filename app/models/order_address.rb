f1674ecf4e6146287fdf4a8efb90b6d513a882ec
shibayamasho <men.in.boss2@gmail.com>
2021-02-23 11:37:52 +0900

    order_address.rb　データを保存する記述

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -11,4 +11,9 @@ class OrderAddress
     validates :address
     validates :phone_number, format: {with: /\A[0-9]{10,11}+\z/}
   end
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, municipalities: municipalities, address: address, building_name: building_name, phone_number: phone_number, order_id: order.id)
+  end
 end
