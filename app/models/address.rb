64671c971997d90e0a7d424b21113cb3fe57945d
shibayamasho <men.in.boss2@gmail.com>
2021-02-23 11:31:52 +0900

    バリデーションの記述をFormオブジェクトパターンに移動

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,12 +1,3 @@
 class Address < ApplicationRecord
-  with_options presence: true do
-    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :prefecture_id, numericality: { other_than: 1 }
-    validates :municipalities
-    validates :address
-    validates :phone_number, format: {with: /\A[0-9]{10,11}+\z/}
-    validates :order_id
-  end
-
   belongs_to :order
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,5 +1,14 @@
 class OrderAddress
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :municipalities, :address, :building_name, :phone_number
 
-
-  
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :prefecture_id, numericality: { other_than: 1 }
+    validates :municipalities
+    validates :address
+    validates :phone_number, format: {with: /\A[0-9]{10,11}+\z/}
+  end
 end
