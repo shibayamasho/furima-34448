0d03167bb61bbbad26664c4ae8c37d7288669035
shibayamasho <men.in.boss2@gmail.com>
2021-02-23 22:05:05 +0900

    itemsのFactoryBotを修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -5,7 +5,7 @@ FactoryBot.define do
     category_id        { Faker::Number.between(from: 2, to: 11) }
     condition_id       { Faker::Number.between(from: 2, to: 7) }
     delivery_fee_id    { Faker::Number.between(from: 2, to: 3) }
-    shipment_source_id { Faker::Number.between(from: 2, to: 48) }
+    prefecture_id      { Faker::Number.between(from: 2, to: 48) }
     days_to_ship_id    { Faker::Number.between(from: 2, to: 4) }
     price              { Faker::Number.between(from: 300, to: 9_999_999) }
     association :user
