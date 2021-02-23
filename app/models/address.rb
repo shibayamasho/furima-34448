ece44412e8541465dd1bf1f44e436ee378f42f32
shibayamasho <men.in.boss2@gmail.com>
2021-02-23 11:17:30 +0900

    prefecture_idにバリデーションを追記

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,7 +1,7 @@
 class Address < ApplicationRecord
   with_options presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :prefecture_id
+    validates :prefecture_id, numericality: { other_than: 1 }
     validates :municipalities
     validates :address
     validates :phone_number, format: {with: /\A[0-9]{10,11}+\z/}
