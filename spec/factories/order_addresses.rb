09938eef12d0b05b7664cbb40f6ad39b783efcd2
shibayamasho <men.in.boss2@gmail.com>
2021-02-24 11:10:48 +0900

    order_addressのFactoryBotの記述を修正

--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -1,6 +1,10 @@
 FactoryBot.define do
   factory :order_address do
-    token          { 'tok_0000000000000000000000000000' }
+    #number         { '4242424242424242' }
+    #exp_month      { '12' }
+    #exp_year       { '25' }
+    #cvc            { '123' }
+    token          { 'tok_1234567890123456789012345678' }
     postal_code    { '000-0000' }
     prefecture_id  { Faker::Number.between(from: 2, to: 48) }
     municipalities { '市町村' }
